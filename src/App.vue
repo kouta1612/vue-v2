bc240c2504c944864a8fccd95cc00102e90326e0
萩原孔太 <kota@hagiharakoufutoshinoMacBook-Pro.local>
2020-11-01 17:14:43 +0900

    動的にURLを変更する

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <nav>
+      <router-link to="/" class="link" active-class="link--active" exact>HOME</router-link>
+      <router-link to="/users" class="link" active-class="link--active" exact>USERS</router-link>
+    </nav>
     <router-view></router-view>  
   </div>  
 </template>
+
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+  .link--active {
+    font-size: 20px;
+  }
+</style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
         <h1>Users</h1>
+        <hr>
+        <h1>User: No. {{ $route.params.id }}</h1>
     </div>
 </template>

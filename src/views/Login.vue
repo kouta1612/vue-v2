722f9d1ca024c556f70efe0e6717fe2f8ecba517
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2020-11-02 13:05:45 +0900

    add login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,9 @@
 </template>
 
 <script>
-export default {
+import axios from '../axios-auth';
+
+export default { 
   data() {
     return {
       email: "",
@@ -21,7 +23,16 @@ export default {
   },
   methods: {
       login() {
-          
+          axios.post(
+              '/accounts:signInWithPassword?key=AIzaSyChAM2pUX-vaU3MZx-Hf75Afs7buf5d2kM',
+              {
+                  email: this.email,
+                  password: this.password,
+                  returnSecureToken: true
+              }
+          ).then(response => {
+            console.log(response);
+          })
       }
   }
 }

e778439fe6c504697618081dc8ca7ccdf8492c8b
萩原孔太 <kota@hagiharakoufutoshinoMacBook-Pro.local>
2020-11-03 11:08:59 +0900

    add redirect to comments page

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from '../axios-auth';
+import router from '../router';
 
 Vue.use(Vuex);
 
@@ -27,6 +28,7 @@ export default new Vuex.Store({
                 }
             ).then(response => {
               commit('updateIdToken', response.data.idToken);
+              router.push('/');
             })
          },
          register({ commit }, authData) {
@@ -39,6 +41,7 @@ export default new Vuex.Store({
                 }
             ).then(response => {
               commit('updateIdToken', response.data.idToken);
+              router.push('/');
             })
          }
     }

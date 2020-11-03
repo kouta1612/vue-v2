f542bd623d50a25a0b9ff0c3a8e3eb71afe5a56e
萩原孔太 <kota@hagiharakoufutoshinoMacBook-Pro.local>
2020-11-03 11:39:50 +0900

    add refresh idToken

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from '../axios-auth';
 import router from '../router';
+import axiosRefresh from '../axios-refresh';
 
 Vue.use(Vuex);
 
@@ -18,7 +19,7 @@ export default new Vuex.Store({
         }
     },
     actions: {
-         login({ commit }, authData) {
+         login({ commit, dispatch }, authData) {
             axios.post(
                 '/accounts:signInWithPassword?key=AIzaSyChAM2pUX-vaU3MZx-Hf75Afs7buf5d2kM',
                 {
@@ -28,8 +29,22 @@ export default new Vuex.Store({
                 }
             ).then(response => {
               commit('updateIdToken', response.data.idToken);
+              setTimeout(() => {
+                dispatch('refreshIdToken', response.data.refreshToken);
+              }, response.data.expiresIn * 1000)
               router.push('/');
             })
+         },
+         refreshIdToken({ commit, dispatch }, refreshToken) {
+            axiosRefresh.post('/token?key=AIzaSyChAM2pUX-vaU3MZx-Hf75Afs7buf5d2kM', {
+                grant_type: 'refresh_token',
+                refresh_token: refreshToken
+            }).then(response => {
+                commit('updateIdToken', response.data.id_token);
+                setTimeout(() => {
+                    dispatch('refreshIdToken', response.data.id_token);
+                }, response.data.expires_in * 1000);
+            });
          },
          register({ commit }, authData) {
             axios.post(
